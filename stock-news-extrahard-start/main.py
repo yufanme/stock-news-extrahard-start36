0c322702f2cd27c39081979c81d56b30252af563
WilliamYu <562937707@qq.com>
2022-01-21 00:37:54 +0800

    finished

--- stock-news-extrahard-start/main.py
+++ stock-news-extrahard-start/main.py
@@ -1,31 +1,30 @@
 import requests
 import datetime as dt
-from newsapi import NewsApiClient
+from twilio.rest import Client
+import smtplib
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+CHANGE = 1
 
-AV_API_KEY = "QJBEJGRF3O7UTNUK"
+ALPHA_VANTAGE_API_KEY = "QJBEJGRF3O7UTNUK"
 ALPHA_VANTAGE_END_POINT = "https://www.alphavantage.co/query"
 
 NEWS_API_END_POINT = "https://newsapi.org/v2/everything"
 NEWSAPI_API_KEY = "1aa76e5062a14bb3a6307d205d8ab3c5"
 
-headers = {
-          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) "
-                        "Chrome/65.0.3325.181 Safari/537.36"
-      }
-proxies = {
-    "https": "https://164.70.118.39:3128",
-    "http": "http://59.21.84.108:80",
-}
+account_sid = "AC8a95a85cabd382f190b3ac17e0a97d26"
+auth_token = "1c531beaff38baf0e0b5b5148a225b61"
+
+EMAIL = "562937707@qq.com"
+PASSWORD = "bpyjiqjylklcbdhe"
 
 # # STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 alpha_vantage_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": "TSLA",
-    "apikey": AV_API_KEY
+    "apikey": ALPHA_VANTAGE_API_KEY
 }
 response = requests.get(ALPHA_VANTAGE_END_POINT, params=alpha_vantage_params)
 response.raise_for_status()
@@ -40,46 +39,55 @@ if month < 10:
     month = f"0{month}"
 year = now.year
 
-yesterday = f"{year}-{month}-{day -1}"
-day_before_yesterday = f"{year}-{month}-{day -2}"
+yesterday = f"{year}-{month}-{day -2}"
+day_before_yesterday = f"{year}-{month}-{day -3}"
 
 yesterday_price = float(alpha_vantage_data["Time Series (Daily)"][yesterday]["4. close"])
 day_before_yesterday_price = float(alpha_vantage_data["Time Series (Daily)"][day_before_yesterday]["4. close"])
 price_change = (yesterday_price-day_before_yesterday_price)/day_before_yesterday_price * 100
-price_change = abs(price_change)
-if price_change > 5:
-    print("get news.")
+if price_change > 0:
+    sign = "up"
+else:
+    sign = "down"
+price_change = round(abs(price_change))
+
+if price_change > CHANGE:
+    # # STEP 2: Use https://newsapi.org
+    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+    newsapi_params = {
+        "apiKey": NEWSAPI_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    response = requests.get(NEWS_API_END_POINT, params=newsapi_params)
+    response.raise_for_status()
+    news = response.json()
+
+    news_data = response.json()
+    articles = news_data["articles"][:3]
+    news_3 = f"TSLA: {sign}{price_change}%\n"
+    for article in articles:
+        news_3 += f"Headline: {article['title']}\n"
+        news_3 += f"Brief: {article['description']}\n"
+    print(news_3)
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            body=f"{news_3}",
+            from_='+16065540848',
+            to='+8619808145773'
+        )
+    print(message.status)
+
+    with smtplib.SMTP("smtp.qq.com") as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL,
+                            to_addrs=EMAIL,
+                            msg=f"Subject:STOCK NEWS\n\n{news_3}")
 else:
     print("change is too small.")
 
-
-# # STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-# newsapi = NewsApiClient(api_key=NEWSAPI_API_KEY)
-# all_articles = newsapi.get_everything(
-#     q="Tesla",
-#     sources='bbc-news,the-verge',
-#     domains='bbc.co.uk,techcrunch.com',
-#     from_param=f'{day_before_yesterday}',
-#     to=f'{yesterday}',
-#     language='en',
-#     sort_by='relevancy',
-#     page=2)
-#
-# response = newsapi.get_sources()
-# print(response)
-# print("end")
-
-newsapi_params = {
-    "apiKey": NEWSAPI_API_KEY,
-    "qInTitle": COMPANY_NAME,
-}
-# response = requests.get(NEWS_API_END_POINT, params=newsapi_params)
-response = requests.get(NEWS_API_END_POINT, params=newsapi_params)
-response.raise_for_status()
-news = response.json()
-
-print(response.status_code)
 
 # # STEP 3: Use https://www.twilio.com
 # Send a separate message with the percentage change and each article's title and description to your phone number.
